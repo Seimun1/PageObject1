9314d69dfcad345d611f80313110aea74d6a3e07
Dina-72 <amenova1109@yandex.ru>
2023-09-01 22:48:00 +0500

    errored second test

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/gmeoapitajr90b4d?svg=true)](https://ci.appveyor.com/project/Dina1109/pageobject)
+
---
+++ src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
@@ -0,0 +1,60 @@
+package ru.netology.pageobject.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.pageobject.page.DashboardPage;
+import ru.netology.pageobject.page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.pageobject.data.DataHelper.*;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    @DisplayName("Should Transfer Money From First Card To Second")
+    void shouldTransferFromFirstToSecond() {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo); //баланс 1-ой карты до перевода
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);//баланс 2-ой карты до перевода
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);//вызов метода выбора карты
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    @DisplayName("Should Get Error Message If Amount More Balance")
+    void shouldGetErrorMessageIfAmountMoreBalance() {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo); //баланс 1-ой карты до перевода
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);//баланс 2-ой карты до перевода
+        var amount = generateValidAmount(secondCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        transferPage.findErrorMessage("На карте недостаточно средств");
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(firstCardBalance, actualBalanceFirstCard);
+        assertEquals(secondCardBalance, actualBalanceSecondCard);
+    }
+
+}
+
